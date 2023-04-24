59bb5307daaede8d939034180cb605cd11a3deb5
sprova03 <sprova03@gmail.com>
2023-04-24 11:26:20 -0300

    Modificacion de estilos de pantalla principal de dinosaurios

--- src/Components/Pages/DinoScreen/DinoScreen.js
+++ src/Components/Pages/DinoScreen/DinoScreen.js
@@ -4,29 +4,51 @@ import { Link, useParams } from 'react-router-dom';
 import { dinosaurios } from '../../../Data/data';
 import { Header } from '../../Header/Header';
 import { DinoScreenCuriosidades } from './DinoScreenCuriosidades';
+import { DinoScreenImg } from './DinoScreenImg';
+import { DinoScreenCualidades } from './DinoScreenCualidades';
 
 
 export const DinoScreen = () => {
   const [dino, setDino] = useState(dinosaurios)
+  const [tipoDieta, setTipoDieta] = useState('')
+
+
+
   const {id} = useParams()
   useEffect(() => {
-    setDino(dino.filter(e => e.id === (id * 1)))
+    const dinos = dino.filter(e => e.id === (id * 1))
+    setDino(dinos)
 
   
 
   }, [])
+
+  useEffect(() => {
+    
+   setTipoDieta(dinosaurios.filter(e => e.id === (id * 1))[0].dieta)
+  })
+  
+
   return (
-    <section className='container-dino-screen'>
+    < >
       <Header/>
-      <div>
-      <h1>
+      <div className='container-dino-screen'>
+        <div className='dino-screen-title'>
+      <h2>
       {dino[0].nombre}
-      </h1>
+      </h2>
       <Link to={'/'}><h3>Volver</h3></Link>
-      </div>
+        </div>
       <h3>{dino[0].descripcion}</h3>
-      <img alt='Foto del dino' src={dino[0].img}/>
-      <DinoScreenCuriosidades data={dino[0]}/>
-      </section>
+      </div>
+      <div className='dino-screen-cualidades'>
+      <DinoScreenCualidades tipoDieta={tipoDieta} data={dino[0]}/>
+      <DinoScreenImg data={dino[0]}/>
+      </div>
+      <div className='dino-screen-curiosidades'>
+        <h3>Curiosidades:</h3>
+      <DinoScreenCuriosidades tipoDieta={tipoDieta} data={dino[0]}/>
+      </div>
+      </>
   )
 }
---
+++ src/Components/Pages/DinoScreen/DinoScreenCualidades.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const DinoScreenCualidades = ({tipoDieta, data}) => {
+  return (
+    <ul className={`dino-screen-lista-cualidades ${tipoDieta}`}>
+    <li  className='dino-screen-lista-item'><span>P</span>eríodo: {data.periodo}</li>
+    <li className='dino-screen-lista-item'><span>U</span>bicación: {data.ubicacionGeografica}</li>
+    <li className='dino-screen-lista-item'><span>D</span>ieta: {data.dieta}</li>
+    <li className='dino-screen-lista-item'><span>T</span>amaño: {data.tamaño}</li>
+    <li className='dino-screen-lista-item'><span>P</span>eso: {data.peso}</li>
+  </ul>
+  )
+}
